95d906c8db87534657575912d2bda516d07005b4
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-10-07 19:18:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,23 @@
 - dodavanje novog vozila
 '''
 import json
+from typing import List
 
 
-FILE_PATH = 'sample_data/trucks_sample_eu.json'
+FILE_PATH = 'sample_data/trucks_sample.json'
 
 
 
+def load_from_json() -> List:
+    try:
+        with open(FILE_PATH, 'r') as file_reader:
+            return json.load(file_reader)
 
-try:
-    with open(FILE_PATH, 'r') as file_reader:
-        data = json.load(file_reader)
-        print(f'Ukupan broj elemenata: {len(data)}')
-        print('Prvi element kolekcije')
-        print(data[0])
+    except Exception as ex:
+        print(f'Dogodila se greska {ex}.')
+        return []
 
-except Exception as ex:
-    print(f'Dogodila se greska {ex}.')
+
+
+data = load_from_json()
+print(data[0] if len(data) > 0 else 'Nema podataka u datoteci')
