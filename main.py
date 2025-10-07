dcec91b424a44b5d7e6fb92c2540146d0b58ff01
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-10-07 18:56:59 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+'''
+- ispis svih vozila
+- ispis detalja jednog vozila
+- dodavanje novog vozila
+'''
+import json
+
+
+FILE_PATH = 'trucks_sample_eu.json'
+
+
+
+
+try:
+    with open(FILE_PATH, 'r') as file_reader:
+        data = json.load(file_reader)
+        print(f'Ukupan broj elemenata: {len(data)}')
+        print('Prvi element kolekcije')
+        print(data[0])
+
+except Exception as ex:
+    print(f'Dogodila se greska {ex}.')
